83eb55025167c83a59c24cff05507d8735151c65
Dostypnyi_Ternopil <dostupnyiternopil@rambler.ru>
2020-07-29 13:07:36 +0300

    props for onclick btn

--- src/components/input/InputComponent.js
+++ src/components/input/InputComponent.js
@@ -11,26 +11,30 @@ const ButtonsWrapper = styled.div`
   width: 424px;
   margin-top: 23px;
 `;
-const Form = styled.form``;
 
 export default function InputComponent(props) {
-  const [NewLink, setNewLink] = useState(null);
-
-  const addNewLink = (e) => {
-    e.preventDefault();
+  const [NewLink, setNewLink] = useState('');
+  const inputValue = (e) => {
+    setNewLink(e.target.value);
     console.log(e.target.value);
-  }
   
+  };
+
+  const handleClick = (e) => {
+    {e} = props;
+    console.log('setNewLink');
+    setNewLink(e.target.value);
+  };
+
+ 
     return (
     <div>
-        <h1>Put your URL</h1>
-        <Form onChange={addNewLink}>
-        <InputField onChange={(e) => setNewLink(e.target.value)}/>
+        <h1>Put your URL</h1>      
+        <InputField value={NewLink} onChange= {inputValue}/>
         <ButtonsWrapper>
-          <Btn text="Add +" background="#636663" type = "submit"  onClick={addNewLink}></Btn>
+        <Btn text="Add +" background="#636663" type = "button"  onClick={props.handleClick}></Btn>
           <Btn text = "Generate" background="#48ED39" width = "180px"></Btn>
-        </ButtonsWrapper>
-        </Form>
+        </ButtonsWrapper>        
     </div>
   );
 }
